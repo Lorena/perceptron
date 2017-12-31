5ac386c0af948a52ac8bbedbc4472cc111a742b2
Lorena Souza <lcsouza@thoughtworks.com>
2017-12-30 23:18:01 -0200

    Refactoring Neuron Fruit and LogicOperator

--- src/rna/perceptron/fruits/NeuronLearningCompositeChemicalOfFruit.java
+++ src/rna/perceptron/fruits/NeuronLearningCompositeChemicalOfFruit.java
@@ -7,19 +7,19 @@ import rna.perceptron.ActivationFunction;
 public class NeuronLearningCompositeChemicalOfFruit implements Perceptron{
 
     private final String SIGMOID_BIPOLAR_FUNCTION = "SIGMOID_BIPOLAR";
-
     private ActivationFunction activationFunction;
+    private TrainFruit train;
 
     @Override
     public void trainingByExamples() {
-        TrainFruit train = new TrainFruit(0, 0.05, SIGMOID_BIPOLAR_FUNCTION);
+        train = new TrainFruit(0, 0.05, SIGMOID_BIPOLAR_FUNCTION);
         train.execute();
-        activationFunction = new ActivationFunction(SIGMOID_BIPOLAR_FUNCTION, 0.0, train.getWeights());
     }
 
     @Override
     public void learningAnExample(Example example, String expected) {
         System.out.println("Executa o aprendizado por Perceptron: ");
+        activationFunction = new ActivationFunction(SIGMOID_BIPOLAR_FUNCTION, 0.0, train.getWeights());
         activationFunction.getResultedFruit(example);
         System.out.println("Expected: " + expected);
         System.out.println("____");
--- src/rna/perceptron/logic/operator/NeuronLearningLogicOperator.java
+++ src/rna/perceptron/logic/operator/NeuronLearningLogicOperator.java
@@ -8,17 +8,18 @@ public class NeuronLearningLogicOperator implements Perceptron {
 
     private final String SIGMOID_FUNCTION = "SIGMOID";
     private ActivationFunction activationFunction;
+    private TrainLogic train;
 
     @Override
     public void trainingByExamples() {
-        TrainLogic train = new TrainLogic(0.3, 0.2, SIGMOID_FUNCTION);
+        train = new TrainLogic(0.3, 0.2, SIGMOID_FUNCTION);
         train.execute();
-        activationFunction = new ActivationFunction(SIGMOID_FUNCTION, 0.3, train.getWeights());
     }
 
     @Override
     public void learningAnExample(Example example, String expected) {
         System.out.println("Executa o aprendizado por Perceptron: ");
+        activationFunction = new ActivationFunction(SIGMOID_FUNCTION, 0.3, train.getWeights());
         activationFunction.getResulted(example);
         System.out.println("Expected: " + expected);
         System.out.println("_____");
